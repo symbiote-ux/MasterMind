bde4af0fe05bc738b5148f33611ff454ee828dd9
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-03-17 02:47:03 +0530

    added msg and about box

--- public/js/gameController.js
+++ public/js/gameController.js
@@ -1,4 +1,14 @@
-const colors = () => [ 'red', 'blue', 'green', 'pink', 'black', 'lime', 'grey', 'orange', 'yellow' ];
+const colors = () => [
+  'red',
+  'blue',
+  'green',
+  'pink',
+  'black',
+  'lime',
+  'grey',
+  'orange',
+  'yellow'
+];
 
 const fillCode = code => {
   const codeBox = Array.from(document.querySelectorAll('.displayCorrectCode'));
@@ -16,7 +26,7 @@ const disableRow = attempt => {
     col.removeEventListener('dragover', allowDrop);
   });
   row.querySelector('.logo').style.pointerEvents = 'none';
-}
+};
 
 const enableRow = (noOfAttempt = 1) => {
   const rowId = `PM_Row_${noOfAttempt}`;
@@ -34,22 +44,30 @@ const displayClue = (rightColor, rightColorAndPos, attempt) => {
   const clueBox = document.getElementById(rowId);
   const colList = Array.from(clueBox.querySelectorAll('.clueCol'));
   for (let i = 0; i < rightColor; i++) {
-    colList[i].style.backgroundColor = i < rightColorAndPos ? 'green' : 'yellow';
+    colList[i].style.backgroundColor =
+      i < rightColorAndPos ? 'green' : 'yellow';
   }
+};
+
+const displayColorAndMsg = (game, msg) => {
+  const msgBox = document.querySelector('#msg');
+  msgBox.innerText = msg;
+  msgBox.style.display = 'block';
+  fillCode(game.keyCode);
 };
 
 const checkCode = function() {
   const row = event.target.parentElement;
   const playerMoves = Array.from(row.querySelectorAll('.col'));
   const playerCode = playerMoves.map(move => move.style.backgroundColor);
-  const { rightColorCount, rightColorAndPosCount, attempt } = this.compareCode( playerCode );
-  if(this.isCodeCracked(rightColorAndPosCount)) {
-    fillCode(this.keyCode);
-    alert('code cracked');
+  const { rightColorCount, rightColorAndPosCount, attempt } = this.compareCode(
+    playerCode
+  );
+  if (this.isCodeCracked(rightColorAndPosCount)) {
+    displayColorAndMsg(this, 'You have Cracked the Code');
   }
-  if(attempt === 17 ){
-    fillCode(this.keyCode);
-    alert('limit reached');
+  if (attempt === 17) {
+    displayColorAndMsg(this, 'Max Attempt Limit Reached');
   }
   displayClue(rightColorCount, rightColorAndPosCount, attempt);
   disableRow(attempt);
@@ -62,7 +80,7 @@ const attachEventListener = game => {
   checkButtons.forEach(button => {
     button.addEventListener('click', checkCode.bind(game));
     button.style.pointerEvents = 'none';
-  })
+  });
 };
 
 const main = () => {
